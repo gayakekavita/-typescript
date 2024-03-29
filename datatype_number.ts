3131b24d5c05f985675476ecaf9e03771c73b8a9
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-29 10:44:36 +0530

    Create datatype_number.ts

---
+++ datatype_number.ts
@@ -0,0 +1,14 @@
+let positivenum:number=10;
+let negativenum:number=-10;
+let pinum:number=3.14;
+console.log("positive number: "+ positivenum);
+console.log("negative Number: "+ negativenum);
+console.log(" Value of pi:" +pinum);
+//following statement will throw error because we are tying to assign string value to number type variable 
+//let num:number="kavi";
+
+let squarevalue=Math.sqrt(16);
+console.log("square root value: "+squarevalue);
+
+let nannum:number= NaN;
+console.log(nannum);
