7a6b055d49a31eceadfffe1c0af4fccb2263cfaa
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-29 10:50:43 +0530

    Create datatype_string.ts

---
+++ datatype_string.ts
@@ -0,0 +1,10 @@
+//anything which is declared inside ' ' and " " is reffered as string in typescript
+
+let myname="kavi";
+let lastname='gayake';
+let greet="hello Everyone !! How Are You";
+console.log("Name:" + myname);
+console.log("Last Name: "+ lastname);
+console.log (greet + " In uppercase : " + greet.toUpperCase());
+console.log (greet + " In lowercase : " + greet.toLowerCase());
+console.log ("Length of greet is : " + greet.length);
